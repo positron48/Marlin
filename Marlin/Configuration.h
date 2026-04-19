39844c1e9276fe6733bc103212589518425dfa04
Filatov Anton <positron48@gmail.com>
2026-04-19 18:43:08 +0300

    Invert extruder direction and enable filament runout sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1687,7 +1687,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
@@ -1806,9 +1806,10 @@
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  */
 //#if DISABLED(SAPPHIRE_PLUS_MKS_UI)
-  //#define FILAMENT_RUNOUT_SENSOR
+  #define FILAMENT_RUNOUT_SENSOR
 //#endif
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
+  #define FIL_RUNOUT_PIN PA4             // MKS Robin Nano v1.2 MT_DET_1
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   1          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
 
