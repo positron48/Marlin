2572799a2d278bcc646a3cdb62874037d53aea97
Filatov Anton <positron48@gmail.com>
2026-04-19 19:13:24 +0300

    Tune extruder steps to 930 based on 50mm test

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1192,7 +1192,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT  { 160, 160, 800, 824 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT  { 160, 160, 800, 930 }
 //#define DEFAULT_AXIS_STEPS_PER_UNIT  {   80, 80, 400, 415 )
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
