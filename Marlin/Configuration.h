a2cec856fd22010142ed908463ac92e0432e4ae0
Filatov Anton <positron48@gmail.com>
2026-04-19 14:00:39 +0300

    Enable MKS LVGL UI and switch Z homing to ZMAX (top endstop)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -23,8 +23,8 @@
 
 #define CONFIG_EXAMPLES_DIR "config/examples/Two Trees/Sapphire Plus/Sapphire Plus V2"
 
-//#define SAPPHIRE_PLUS_MKS_UI
-#define SAPPHIRE_PLUS_BLTOUCH
+#define SAPPHIRE_PLUS_MKS_UI
+//#define SAPPHIRE_PLUS_BLTOUCH
 //#define SAPPHIRE_PLUS_TMC_UART
 
 /**
@@ -1042,7 +1042,7 @@
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
 //#define USE_YMIN_PLUG
-#define USE_ZMIN_PLUG
+//#define USE_ZMIN_PLUG
 //#define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
 //#define USE_KMIN_PLUG
@@ -1052,7 +1052,7 @@
 //#define USE_XMAX_PLUG
 #define USE_YMAX_PLUG
 #if DISABLED(SAPPHIRE_PLUS_BLTOUCH)
-//  #define USE_ZMAX_PLUG
+  #define USE_ZMAX_PLUG
 #endif
 //#define USE_IMAX_PLUG
 //#define USE_JMAX_PLUG
@@ -1717,7 +1717,7 @@
 // :[-1,1]
 #define X_HOME_DIR -1
 #define Y_HOME_DIR 1
-#define Z_HOME_DIR -1
+#define Z_HOME_DIR 1
 //#define I_HOME_DIR -1
 //#define J_HOME_DIR -1
 //#define K_HOME_DIR -1
